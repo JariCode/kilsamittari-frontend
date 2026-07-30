3cd3d2ff4eae8f0379ed35e79962be4921f2a733
Jari Karri <jari.karri@gmail.com>
2026-07-30 20:48:12 +0300

    Lisätty alapolun /kilsamittari tuki, toimii sekä paikallisesti että tuotannossa

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
+// Reitityksen juuri luetaan Viten base-arvosta, jotta toimii sekä paikallisesti että alapolussa
+// Kehityksessä tämä on tyhjä, tuotannossa /kilsamittari
+const basename = import.meta.env.BASE_URL.replace(/\/$/, '');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
